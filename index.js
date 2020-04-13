8e147e9680df39e6f588a58a244e116b186ac8b4
Vanda Adams <vandamadams@gmail.com>
2020-04-13 10:47:38 +0100

    updated top&bottom positioning on smaller screens

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function makeMarquee() {
   const title = 'HAMAROSAN NYITUNK! — KÉZMŰVES FAGYLALT HÁZHOZSZÁLLÍTÁS'
-  const marqueeText = new Array(320).fill(title)
+  const marqueeText = new Array(150).fill(title)
   const marquee = document.querySelector('.marquee span')
   marquee.innerHTML = marqueeText.join(' — ')
 }
